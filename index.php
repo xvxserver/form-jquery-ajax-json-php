ccc5c62f1c2103e0c158212bf1e5baff49b2cfee
xvxserver <xvxserver@gmail.com>
2016-03-02 13:28:46 -0430

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>document</title>
+	<link rel="stylesheet" href="style.css">
+	<script src="jquery.js"></script>
+	<script src="function.js"></script>
+</head>
+<body>
+
+<section>
+
+	<form id="form_1" method="post" autocomplete="off" action="validate.php">
+		<input id="username" placeholder="Usuario" type="text" name="username"></input><br />
+		<input id="password" placeholder="Contraseña" type="text" name="password"></input><br />
+		<input id="submit_1" type="submit" value="Conectar">
+	</form>
+
+	<div id="status_1" style="display: none">
+		<div class="success">
+		<progress id="progressbar_1" value="0" max="100"></progress>
+		</div>
+		<div id="result_1"></div>
+	</div>
+
+</section>
+
+</body>
+</html>
